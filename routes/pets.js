3a0557932f7a1085b4f9cce811ac4afd6cca42b5
“jelin.mi” <jelin.mi@gmail.com>
2021-12-26 19:41:06 +0100

    After adding a pet, the redirect does not work. Should be /user-pets-list, not /zoo/add/zoo/add

--- routes/pets.js
+++ routes/pets.js
@@ -12,11 +12,11 @@ const User = require('../models/user');
 function petsRoutes() {
   const router = express.Router();
 
- /*  router.get('/zoo/add', (req, res, next) => {
+  /*  router.get('/zoo/add', (req, res, next) => {
     res.render('pets/create-pet');
-  }); */
+}); */
 
- /*  How to link it with a user? Something like this below? */
+  /*  How to link it with a user? Something like this below? */
   router.get('/zoo/add', (req, res, next) => {
     User.find().then(usersFromDB => {
       res.render('pets/create-pet', { usersFromDB });
@@ -24,12 +24,48 @@ function petsRoutes() {
   });
 
   // POST
-    router.post('/zoo/add', (req, res, next) => {
+  router.post('/zoo/add', (req, res, next) => {
     const { petsName, owner, sex, age, color } = req.body;
 
     Pet.create({ petsName, owner, sex, age, color })
+      .then(dbPet => {
+        return User.findByIdAndUpdate(owner, { $push: { pets: dbPet._id } });
+      })
       .then(() => res.redirect('/user-pets-list'))
-      .catch(() => res.render('pets/create-pet'));
+      /* .catch(() => res.render('pets/create-pet')); */
+      .catch(err => {
+        console.log(`Err while creating the pet in the DB: ${err}`);
+        next(err);
+      });
+  });
+
+  // This is copied from Learning Unit, it needs to be adapted - other way round as we have ObjectId in  Pet model, not in a User model ??
+  
+  // READ - user-pets-list
+  router.get('/user-pets-list', (req, res, next) => {
+    Pet.find()
+      .populate('owner')
+      .then(dbPets => {
+        console.log('Pets from the DB:', dbPets);
+        res.render('pets/list', { pets: dbPets });
+      })
+      .catch(err => {
+        console.log(`Err while getting the pets from the DB: ${err}`);
+        next(err);
+      });
+  });
+
+  // Pet details page = Pet profile
+  router.get('/zoo/:petId', (req, res, next) => {
+    const { petId } = req.params;
+
+    Pet.findById(petId)
+      .populate('owner')
+      .then(foundPet => res.render('pets/profile', foundPet))
+      .catch(err => {
+        console.log(`Err while getting a single post from the  DB: ${err}`);
+        next(err);
+      });
   });
 
   return router;
