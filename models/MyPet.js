6c6725237a29cdf202cff4aeb2c7d24482226fcc
“jelin.mi” <jelin.mi@gmail.com>
2021-12-29 21:34:19 +0100

    the changes on michaela/model  accepted when conflict are now commented

--- models/MyPet.js
+++ models/MyPet.js
@@ -3,26 +3,27 @@ const { Schema, model } = require('mongoose');
 const myPetSchema = new Schema({
   // MyPet model
   owner: { type: Schema.Types.ObjectId, ref: 'owner' },
-  //pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
-  petsName: {
-  owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
+  // pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
+
+  /*  petsName: {
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    pet: { type: Schema.Types.ObjectId, ref: 'Pet' },
+  }, */
 
   // I think the info below is not needed, it will be taken from Pet model.
-  /* petsName: {
+  petsName: {
     type: String,
     required: true,
   },
   sex: String,
   age: Number,
-  color: String, */
-}
+  color: String,
 });
 
 // COMPOUND INDEXES
 // To not to add a pet twice to the database:
 // Also, it will first find the owner in ascending order, then it will look for the pet.
-myPetSchema.index({ owner: 1, pet: 1 }, { unique: true });
+// myPetSchema.index({ owner: 1, pet: 1 }, { unique: true });
 
 const MyPet = model('MyPet', myPetSchema);
 
